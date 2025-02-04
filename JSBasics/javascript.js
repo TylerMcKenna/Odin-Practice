3881e1b5dc9f654e2d291dc12324711c33a736bd
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-03 22:51:12 -0600

    Complete a lesson

--- JSBasics/javascript.js
+++ JSBasics/javascript.js
@@ -1,13 +1,21 @@
-let firstname = "John";
-let surname = "Doe";
+console.log(23 + 97 + 2 + 3 + 1 + 3)
 
-console.log(firstname);
-console.log(surname);
+console.log((4 + 6 + 9) / 77)
 
-let age = 11;
+let a = 10
 
-console.log(age);
+console.log(a)
 
-age = 54;
+a = 20
 
-console.log(age);
+console.log(a)
+
+b = 7 * a
+console.log(b)
+
+const max = 57
+
+const actual = max - 13
+
+const percentage = actual / max
+console.log(percentage)
