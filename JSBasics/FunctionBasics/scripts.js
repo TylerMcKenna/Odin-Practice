73597229260a91dfca6fc262cb3d6755b31cd86a
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2025-02-10 20:00:08 -0600

    Complete Light Work
    That's too easy. They don't know me son.

---
+++ JSBasics/FunctionBasics/scripts.js
@@ -0,0 +1,15 @@
+function add7(num) {
+    return num + 7;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2; 
+}
+
+function capitalize(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function lastLetter(text) {
+    return text.charAt(text.length - 1);
+}
